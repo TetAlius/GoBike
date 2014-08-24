4c513c46a7d7d2ca9f89578a4379e45e622e1f74
Álex Sáez <alexsaezm@gmail.com>
2014-08-24 22:52:16 +0200

    Fix

--- backend/backend.go
+++ backend/backend.go
@@ -2,10 +2,7 @@ package backend
 
 import "appengine"
 
-func main() {}
-hostname, err := appengine.ModuleHostname(c, "my-backend", "", "")
-if err != nil {
-
-}
-url = "http://" + hostname + "/"
+func main() {
+	hostname, err := appengine.ModuleHostname(c, "my-backend", "", "")
+	url = "http://" + hostname + "/"
 }
--- frontend/frontend.go
+++ frontend/frontend.go
@@ -10,6 +10,7 @@ import (
 
 func init() {
 	http.HandleFunc("/", handler)
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
