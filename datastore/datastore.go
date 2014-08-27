3b3b11f72ce62c098c650cfff9170fccf91f7470
DiiSandoval <diimieres@gmail.com>
2014-08-28 00:53:10 +0200

    Routes struct &  getAllRoutes(Context context)

---
+++ datastore/datastore.go
@@ -0,0 +1,27 @@
+package datastore
+
+import (
+	"time"
+)
+
+type Route struct {
+	title          string
+	description    string
+	creationDate   time.Time
+	distance       float64
+	beginLoc       string
+	endLoc         string
+	difficulty     string
+	road           bool
+	mountain       bool
+	path           bool
+	comments       []string
+	author         string
+	maps           string
+	duration       time.Time
+	slope          float64
+	photos         string
+	score          string
+	signal         bool
+	beginTransport bool
+}
