b633f099299a23486a3e50a1b27fac1ed58c255b
DiiSandoval <diimieres@gmail.com>
2014-08-28 10:15:55 +0200

    getAllRoutes(...)

--- datastore/routes.go
+++ datastore/routes.go
@@ -1,1 +1,11 @@
-package routes
+package datastore
+
+func getAllRoutes(context Context) []Route {
+	query := datastore.NewQuery("routes")
+	var routes []Route
+	_, error := query.GetAll(context, routes)
+	if error == nil {
+		return routes
+	}
+
+}
