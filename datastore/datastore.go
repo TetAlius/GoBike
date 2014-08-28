f5b724709e64c31a008a86847facef63a8ba5b8b
Álex Sáez <alexsaezm@gmail.com>
2014-08-28 19:03:21 +0200

    changes

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,35 +2,16 @@ package datastore
 
 import (
 	"datastore/maped"
-	"time"
 )
 
-// Route the routes
-type Route struct {
-	title          string
-	description    string
-	creationDate   time.Time
-	distance       float64
-	beginLoc       string
-	endLoc         string
-	difficulty     string
-	road           bool
-	mountain       bool
-	path           bool
-	comments       []string
-	author         string
-	maps           string
-	duration       time.Time
-	slope          float64
-	photos         string
-	score          string
-	signal         bool
-	beginTransport bool
+
+func getAllRoutes(context Context) []Route {
+	query := datastore.NewQuery("routes")
+	var routes []Route
+	_, error := query.GetAll(context, routes)
+	if error == nil {
+		return routes
+	}
+
 }
 
-// User the users
-type User struct {
-	name        string    // the user name
-	since       time.Time // the day when the account was created
-	alreadyDone []Route
-}
