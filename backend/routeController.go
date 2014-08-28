50ec3c98b8a867a5d0b6d9e3b23805ad39367f3a
Menti <josementi@gmail.com>
2014-08-29 00:48:07 +0200

    Para probar en local, sacar todo el proyecto del GOPATH.

--- backend/routeController.go
+++ backend/routeController.go
@@ -3,14 +3,17 @@ package backend
 import (
 	"errors"
 
+	"backend/maped"
+
+	"appengine"
 	"appengine/datastore"
 )
 
 //GetAllRoutes Returns all the routes in the DB
-func GetAllRoutes(context Context) (routes []Route, err error) {
+func GetAllRoutes(context appengine.Context) (routes []maped.Route, err error) {
 	query := datastore.NewQuery("routes")
-	_, err := query.GetAll(context, routes)
-	if error != nil {
+	_, err = query.GetAll(context, routes)
+	if err != nil {
 		context.Errorf("Can´t load the routes: %e", err)
 		err = errors.New("Can't load the routes")
 	}
