d186901daccf8819b9b934ce65fb864b21e946a3
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2014-09-02 14:38:54 +0200

    Form for Register and other fixes
    some part of #24
    Have to validate the form :)

--- frontend/api/main.go
+++ frontend/api/main.go
@@ -29,10 +29,20 @@ func init() {
 	http.HandleFunc("/route/", singleRouteHandler)
 	http.HandleFunc("/insert", insertRoutesHandler)
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 	http.ServeFile(w, r, "./frontend/resources/html/notfound.html")
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("./frontend/resources/html/register.html", "./frontend/resources/html/meta.html", "./frontend/resources/html/login.html")
+	if err != nil {
+		log.Fatal("Parse file error: ", err)
+	}
+	t.Execute(w, map[string]string{"PageTitle": "GoBike - Register"})
 }
 
 func routesHandler(w http.ResponseWriter, r *http.Request) {
