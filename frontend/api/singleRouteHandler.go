405ed11c1825105de794429721e50fff2e6b6aee
Álex Sáez <alexsaezm@gmail.com>
2014-10-12 18:54:42 +0200

    singleRouteHandler

--- frontend/api/singleRouteHandler.go
+++ frontend/api/singleRouteHandler.go
@@ -1,17 +1,16 @@
 package frontend
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func singleRouteHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/route/"):]
-	/*
-	   t, err := template.ParseFiles("./frontend/resources/html/route-template.html")
-	   if err != nil {
-	     log.Fatal("Parse file error: ", err)
-	   }
-	   t.Execute(w, title)*/
-	fmt.Fprintf(w, title)
+	//title := r.URL.Path[len("/route/"):]
+	t, err := template.ParseFiles("./frontend/resources/html/routeDetails.html", "./frontend/resources/html/meta.html", "./frontend/resources/html/login.html")
+	if err != nil {
+		log.Fatal("Parse file error: ", err)
+	}
+	t.Execute(w, map[string]string{"PageTitle": "GoBike - Route"})
 }
